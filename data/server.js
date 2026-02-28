d8a57cde270f9e54282ea11f350714b9a41d5652
thebullvajpai <vaajpaai@gmail.com>
2026-02-28 21:55:56 +0530

    Backend ready for Render deployment

--- data/server.js
+++ data/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { getAIResponse } from "./aiService.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -28,6 +30,8 @@ app.post("/chat", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
